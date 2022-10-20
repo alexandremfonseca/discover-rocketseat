a8298342036423b9c6f54ef9708ff02b9349f590
Alexandre Mucarzel Fonseca <amfonseca98@gmail.com>
2022-10-20 18:20:08 -0300

    Continuing APIs module

---
+++ 6_apis/3_fetch/main.js
@@ -0,0 +1,75 @@
+const url ="http://localhost:5500/api"
+
+const newUser = {
+    name: "Olivia Zars",
+    avatar: "http://picsum.photos/200/300",
+    city: "Rio do Sul"
+}
+
+const updatedUser = {
+    name: "Mayk Brito",
+    avatar: "https://avatars.githubusercontent.com/u/6643122?v=4",
+    city: "Curitiba"
+}
+
+function getUsers() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .catch(error => console.error(error))
+}
+
+function getUser(id) {
+    fetch(`${url}/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
+
+function addUser(newUser) {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+function updateUser(updatedUser, id) {
+    fetch(`${url}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+function deleteUser(id) {
+    fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+// addUser(newUser)
+// updateUser(updatedUser, 9)
+// deleteUser()
+getUsers()
+getUser(9)
